04d66ad86769fd605e885871e9f7dc89bb92f1bc
Md. Shofiqul Islam <shofiqebr@gmail.com>
2024-04-25 16:49:40 +0600

    added recommended part

---
+++ src/pages/home/recomended/Recomended.jsx
@@ -0,0 +1,13 @@
+import SectionTitle from "../../../components/sectionTitle/SectionTitle";
+
+
+const Recomended = () => {
+    return (
+        <section>
+             <SectionTitle heading={'Our Company Recomendations'} subHeading={'Checkout'}></SectionTitle>
+            
+        </section>
+    );
+};
+
+export default Recomended;
