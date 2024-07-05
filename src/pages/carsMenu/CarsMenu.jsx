b95289493beeae984516cecf32112ef4145ba803
Md. shofiqul islam <shofiqebr@gmail.com>
2024-07-05 06:56:46 +0600

    added cover page

---
+++ src/pages/carsMenu/CarsMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cover from '../shared/cover/Cover';
+
+
+const CarsMenu = () => {
+    return (
+        <div>
+            <Cover img={menuImg}></Cover>
+        </div>
+    );
+};
+
+export default CarsMenu;
