fde88e351d863704370393381aa62fa8beafcc3e
shofiq <shofiqebr@gmail.com>
2024-07-25 12:44:06 +0600

    changing some design

--- src/pages/shared/carItem/CarItem.jsx
+++ src/pages/shared/carItem/CarItem.jsx
@@ -3,14 +3,14 @@
 const CarItem = ({item}) => {
     const {name, photo, price, ShortDescription} = item;
     return (
-        <div className="flex space-x-2  ">
-            <img style={{borderRadius: '10px 50px 10px 50px'}} className="w-24 h-24" src={photo} alt="" />
-            <div>
+        <div className="flex space-x-5  ">
+            <img style={{borderRadius: '10px 50px 10px 50px'}} className="w-56 h-56" src={photo} alt="" />
+            <div className="pt-10">
 
-            <h3 className="uppercase font-medium">{name}----------</h3>
-            <p>{ShortDescription}</p>
+            <h3 className="uppercase  text-3xl font-semibold">{name}----------</h3>
+            <p className="font-semibold">{ShortDescription}</p>
+            <p className="text-orange-600 text-xl font-bold">{price}</p>
             </div>
-            <p className="text-yellow-500">{price}</p>
             
         </div>
     );
