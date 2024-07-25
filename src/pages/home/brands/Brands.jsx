37947bc35cdb16e54ac8e832c5d0f19446ecca0a
shofiq <shofiqebr@gmail.com>
2024-07-25 11:53:01 +0600

    added brands card

--- src/pages/home/brands/Brands.jsx
+++ src/pages/home/brands/Brands.jsx
@@ -14,7 +14,7 @@ const Brands = () => {
         <div className='flex flex-col justify-center items-center my-10 gap-10 '>
             <SectionTitle heading={'Most Wanted'} subHeading={'here is our'} />
           
-            <section className='h-44 w-[700px] rounded-xl bg-slate-800 sticky top-[100px] mb-[300px]'>
+            <section className='h-44 w-[700px] rounded-xl  sticky top-[100px] mb-[550px]'>
                 <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-[#0C2A63]">
                     1
                 </div>
@@ -22,7 +22,7 @@ const Brands = () => {
                     Details
                 </div>
                 <div>
-                    <img className='rounded-xl border-4 border-[#0C2A63]' src={awesome1} alt="" />
+                    <img className='h-[450px] w-full  rounded-xl border-4 border-[#0C2A63]' src={awesome1} alt="" />
                     
                 </div>
 
@@ -30,39 +30,39 @@ const Brands = () => {
             </section>
            
            
-            <section className='h-44 w-[700px] rounded-xl bg-teal-200 sticky top-[200px] mb-[200px] -mt-[300px] '>
-            <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-[#0C2A63]">
+            <section className='h-44 w-[700px] rounded-xl  sticky top-[200px] mb-[500px] -mt-[300px] '>
+            <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-orange-600">
                     2
                 </div>
-            <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-[#0C2A63]">
+            <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-orange-600">
             Details
                 </div>
-                <img className='rounded-xl border-4 border-[#0C2A63]' src={awesome2} alt="" />
+                <img className='h-[450px] w-full  rounded-xl border-4 border-orange-600' src={awesome2} alt="" />
 
             </section>
            
            
-            <section className='h-44 w-[700px] rounded-xl bg-indigo-500 sticky top-[300px] mb-[100px] -mt-[200px]'>
+            <section className='h-44 w-[700px] rounded-xl bg-indigo-500 sticky top-[300px] mb-[450px] -mt-[200px]'>
             <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-[#0C2A63]">
                     3
                 </div>
             <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-[#0C2A63]">
             Details
                 </div>
-                <img className='rounded-xl border-4 border-[#0C2A63]' src={awesome3} alt="" />
+                <img className='h-[450px] w-full  rounded-xl border-4 border-[#0C2A63]' src={awesome3} alt="" />
 
             </section>
             
           
           
-            <section className='h-44 w-[700px] rounded-xl bg-sky-900 sticky top-[400px] mb-0 -mt-[100px]'>
-            <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-[#0C2A63]">
+            <section className='h-44 w-[700px] rounded-xl bg-sky-900 sticky top-[500px] mb-[400px] -mt-[100px]'>
+            <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-orange-600">
                     4
                 </div>
-            <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-[#0C2A63]">
+            <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-orange-600">
             Details
                 </div>
-                <img className='rounded-xl border-4 border-[#0C2A63]' src={awesome4} alt="" />
+                <img  className='h-[450px] w-full rounded-xl border-4 border-orange-600' src={awesome4} alt="" />
             </section>
         </div>
     );
