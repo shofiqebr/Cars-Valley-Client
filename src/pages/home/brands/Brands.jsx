bece1e6342e19da414c9bf8ffd52b8bea7fa9e0e
shofiq <shofiqebr@gmail.com>
2024-07-25 13:20:13 +0600

    changing some design

--- src/pages/home/brands/Brands.jsx
+++ src/pages/home/brands/Brands.jsx
@@ -14,11 +14,11 @@ const Brands = () => {
         <div className='flex flex-col justify-center items-center my-10 gap-10 '>
             <SectionTitle heading={'Most Wanted'} subHeading={'here is our'} />
           
-            <section className='h-44 w-[700px] rounded-xl  sticky top-[100px] mb-[550px]'>
-                <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-[#0C2A63]">
+            <section className='md:h-44 md:w-[700px] rounded-xl  sticky top-[100px] mb-[550px]'>
+                <div className="absolute hidden md:block top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-[#0C2A63]">
                     1
                 </div>
-                <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-[#0C2A63]">
+                <div className="absolute hidden md:block top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-[#0C2A63]">
                     Details
                 </div>
                 <div>
@@ -30,11 +30,11 @@ const Brands = () => {
             </section>
            
            
-            <section className='h-44 w-[700px] rounded-xl  sticky top-[200px] mb-[500px] -mt-[300px] '>
-            <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-orange-600">
+            <section className='md:h-44 md:w-[700px] rounded-xl  sticky top-[200px] mb-[500px] -mt-[300px] '>
+            <div className="absolute hidden md:block top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-orange-600">
                     2
                 </div>
-            <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-orange-600">
+            <div className="absolute hidden md:block top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-orange-600">
             Details
                 </div>
                 <img className='h-[450px] w-full  rounded-xl border-4 border-orange-600' src={awesome2} alt="" />
@@ -42,11 +42,11 @@ const Brands = () => {
             </section>
            
            
-            <section className='h-44 w-[700px] rounded-xl bg-indigo-500 sticky top-[300px] mb-[450px] -mt-[200px]'>
-            <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-[#0C2A63]">
+            <section className='md:h-44 md:w-[700px] rounded-xl bg-indigo-500 sticky top-[300px] mb-[450px] -mt-[200px]'>
+            <div className="absolute hidden md:block top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-[#0C2A63]">
                     3
                 </div>
-            <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-[#0C2A63]">
+            <div className="absolute hidden md:block top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-[#0C2A63]">
             Details
                 </div>
                 <img className='h-[450px] w-full  rounded-xl border-4 border-[#0C2A63]' src={awesome3} alt="" />
@@ -55,11 +55,11 @@ const Brands = () => {
             
           
           
-            <section className='h-44 w-[700px] rounded-xl bg-sky-900 sticky top-[500px] mb-[400px] -mt-[100px]'>
-            <div className="absolute top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-orange-600">
+            <section className='md:h-44 md:w-[700px] rounded-xl bg-sky-900 sticky top-[500px] mb-[400px] -mt-[100px]'>
+            <div className="absolute hidden md:block top-16  -left-96 w-10 h-10 text-center pt-2 text-white bg-orange-600">
                     4
                 </div>
-            <div className="absolute top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-orange-600">
+            <div className="absolute hidden md:block top-16  -right-96 w-20 h-10 text-center pt-2 text-white bg-orange-600">
             Details
                 </div>
                 <img  className='h-[450px] w-full rounded-xl border-4 border-orange-600' src={awesome4} alt="" />
--- src/pages/shared/carItem/CarItem.jsx
+++ src/pages/shared/carItem/CarItem.jsx
@@ -3,11 +3,11 @@
 const CarItem = ({item}) => {
     const {name, photo, price, ShortDescription} = item;
     return (
-        <div className="flex space-x-5  ">
-            <img style={{borderRadius: '10px 50px 10px 50px'}} className="w-56 h-56" src={photo} alt="" />
-            <div className="pt-10">
+        <div className="flex space-x-5 justify-center items-center  ">
+            <img style={{borderRadius: '10px 50px 10px 50px'}} className="md:w-56 w-32 md:h-56 h-32" src={photo} alt="" />
+            <div className="md:pt-10">
 
-            <h3 className="uppercase  text-3xl font-semibold">{name}----------</h3>
+            <h3 className="uppercase  md:text-3xl font-semibold">{name}----------</h3>
             <p className="font-semibold">{ShortDescription}</p>
             <p className="text-orange-600 text-xl font-bold">{price}</p>
             </div>
